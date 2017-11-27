e3fa76eaeb32ccec09338952ce222e6ac1e13dcb
edbfool <abrasax08@gmail.com>
2017-11-27 01:02:09 +0100

    react install webpack first setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    render() {
+        return <h3 style={{textAlign: 'center'}}>Hello there !</h3>
+    }
+}
